41505510043bf3719f6b342d278a79d54d932010
Brandon <37314885+Sycendin@users.noreply.github.com>
2022-06-27 02:25:21 -0400

    feat: images switch on mouse enter and add links Project images will switch on mouse/touch enter/leave and add links to demo/source code

--- src/Home/Sections/Projects/Projects.js
+++ src/Home/Sections/Projects/Projects.js
@@ -1,6 +1,36 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 import "./Projects.css";
 const Projects = () => {
+  const [img1, setImg1] = useState(
+    "https://cdn.discordapp.com/attachments/788247984517283880/990860512600399912/gamep1.webp"
+  );
+  const [img2, setImg2] = useState(
+    "https://cdn.discordapp.com/attachments/788247984517283880/990860513086951444/promop1.webp"
+  );
+  // Switch between images depending on mouse enter/leave
+  const imgSwitch = (img, mouse) => {
+    if (img === 1) {
+      if (mouse === "enter") {
+        setImg1(
+          "https://cdn.discordapp.com/attachments/788247984517283880/990860512847872011/gamep2.webp"
+        );
+      } else {
+        setImg1(
+          "https://cdn.discordapp.com/attachments/788247984517283880/990860512600399912/gamep1.webp"
+        );
+      }
+    } else {
+      if (mouse === "enter") {
+        setImg2(
+          "https://cdn.discordapp.com/attachments/788247984517283880/990860513300852756/promop2.webp"
+        );
+      } else {
+        setImg2(
+          "https://cdn.discordapp.com/attachments/788247984517283880/990860513086951444/promop1.webp"
+        );
+      }
+    }
+  };
   return (
     <Fragment>
       <div className="projects-section-div">
@@ -9,37 +39,73 @@ const Projects = () => {
         </div>
         <div className="projects-div">
           <div className="project-div">
-            <p className="project-title">Project 1</p>
+            <p className="project-title">Yu-Gi-Oh! Guessing Game</p>
             <p className="project-info">Project 1 info</p>
             <div className="project-button-div">
-              <button className="project-button">See Live</button>
-              <button className="project-button">Source Code</button>
+              <button
+                className="project-button"
+                onClick={() =>
+                  window.open("https://sycendin.github.io/game/game/", "_blank")
+                }
+              >
+                See Live
+              </button>
+              <button
+                className="project-button"
+                onClick={() =>
+                  window.open("https://github.com/Sycendin/game/", "_blank")
+                }
+              >
+                Source Code
+              </button>
             </div>
           </div>
           <img
+            onMouseEnter={() => imgSwitch(1, "enter")}
+            onMouseLeave={() => imgSwitch(1, "leave")}
+            onTouchStart={() => imgSwitch(1, "enter")}
+            onTouchEnd={() => imgSwitch(1, "leave")}
             alt="project"
             className="project-img"
             height={1080}
             width={1920}
-            src="https://wallpaperset.com/w/full/0/c/4/198595.jpg"
+            src={img1}
           />
         </div>
         <br />
         <div className="projects-div">
           <div className="project-div">
-            <p className="project-title">Project 2</p>
+            <p className="project-title">Toronto Blue Jays Splash Page</p>
             <p className="project-info">Project 2 info</p>
             <div className="project-button-div">
-              <button className="project-button">See Live</button>
-              <button className="project-button">Source Code</button>
+              <button
+                className="project-button"
+                onClick={() =>
+                  window.open("https://github.com/Sycendin/promo/", "_blank")
+                }
+              >
+                See Live
+              </button>
+              <button
+                className="project-button"
+                onClick={() =>
+                  window.open("https://sycendin.github.io/promo/", "_blank")
+                }
+              >
+                Source Code
+              </button>
             </div>
           </div>
           <img
+            onMouseEnter={() => imgSwitch(2, "enter")}
+            onMouseLeave={() => imgSwitch(2, "leave")}
+            onTouchStart={() => imgSwitch(2, "enter")}
+            onTouchEnd={() => imgSwitch(2, "leave")}
             alt="project"
             className="project-img"
             height={1080}
             width={1920}
-            src="https://wallpaperset.com/w/full/0/c/4/198595.jpg"
+            src={img2}
           />
         </div>
         <br />

c53a279f4aaf095ac0c940c66b1c375897acbc27
Brandon <37314885+Sycendin@users.noreply.github.com>
2022-06-30 01:54:08 -0400

    update: add third project

--- src/Home/Sections/Projects/Projects.js
+++ src/Home/Sections/Projects/Projects.js
@@ -8,6 +8,9 @@ const Projects = () => {
   const [img2, setImg2] = useState(
     "https://cdn.discordapp.com/attachments/788247984517283880/990860513086951444/promop1.webp"
   );
+  const [img3, setImg3] = useState(
+    "https://cdn.discordapp.com/attachments/788247984517283880/990884108550959124/newsp1.webp"
+  );
   // Switch between images depending on mouse enter/leave
   const imgSwitch = (img, mouse) => {
     if (img === 1) {
@@ -20,7 +23,7 @@ const Projects = () => {
           "https://cdn.discordapp.com/attachments/788247984517283880/990860512600399912/gamep1.webp"
         );
       }
-    } else {
+    } else if (img === 2) {
       if (mouse === "enter") {
         setImg2(
           "https://cdn.discordapp.com/attachments/788247984517283880/990860513300852756/promop2.webp"
@@ -28,6 +31,16 @@ const Projects = () => {
       } else {
         setImg2(
           "https://cdn.discordapp.com/attachments/788247984517283880/990860513086951444/promop1.webp"
+        );
+      }
+    } else if (img === 3) {
+      if (mouse === "enter") {
+        setImg3(
+          "https://cdn.discordapp.com/attachments/788247984517283880/990884108311875654/newsp2.webp"
+        );
+      } else {
+        setImg3(
+          "https://cdn.discordapp.com/attachments/788247984517283880/990884108550959124/newsp1.webp"
         );
       }
     }
@@ -125,6 +138,48 @@ const Projects = () => {
               height={1080}
               width={1920}
               src={img2}
+            />
+          </div>
+        </Slide>
+        <br />
+        <Slide triggerOnce duration={3000}>
+          <div className="projects-div">
+            <div className="project-div">
+              <p className="project-title">Mock Video Game News Site</p>
+              <p className="project-info">
+                A Mock news site about video games.
+                <br />
+                It uses redux instead of hooks and also uses react-bootstrap
+              </p>
+              <div className="project-button-div">
+                <button
+                  className="project-button"
+                  onClick={() =>
+                    window.open("https://github.com/Sycendin/news/", "_blank")
+                  }
+                >
+                  See Live
+                </button>
+                <button
+                  className="project-button"
+                  onClick={() =>
+                    window.open("https://sycendin.github.io/news/", "_blank")
+                  }
+                >
+                  Source Code
+                </button>
+              </div>
+            </div>
+            <img
+              onMouseEnter={() => imgSwitch(3, "enter")}
+              onMouseLeave={() => imgSwitch(3, "leave")}
+              onTouchStart={() => imgSwitch(3, "enter")}
+              onTouchEnd={() => imgSwitch(3, "leave")}
+              alt="project"
+              className="project-img"
+              height={1080}
+              width={1920}
+              src={img3}
             />
           </div>
         </Slide>

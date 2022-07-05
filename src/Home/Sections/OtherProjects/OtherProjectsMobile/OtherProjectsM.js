a49bc572a99054d7f9dab1b302d34a8b0895df25
Brandon <37314885+Sycendin@users.noreply.github.com>
2022-07-05 04:11:46 -0400

    feat: add mobile view for other projects Other projects section will shift into a more condensed view, improve style for the consdensed view

--- src/Home/Sections/OtherProjects/OtherProjectsMobile/OtherProjectsM.js
+++ src/Home/Sections/OtherProjects/OtherProjectsMobile/OtherProjectsM.js
@@ -3,8 +3,34 @@ import { data } from "./OtherProjectsData.js";
 import "./OtherProjectsM.css";
 const OtherProjectsM = () => {
   const [select, setSelect] = useState(0);
+  const enter = (event) => {
+    event.target.style.backgroundPosition = "0, 0";
+  };
+  const close = (event) => {
+    event.target.style.backgroundPosition = "100% 0";
+  };
+  const changeSelect = (event) => {
+    let button = parseInt(event.target.innerText);
+    document.querySelectorAll(".select").forEach(function (button) {
+      // Set all buttons to default color
+      button.style.background =
+        "linear-gradient(to right, #1babff 0%, #41c4ce 50%, rgb(144, 168, 177) 50%, rgb(144, 168, 177) 100%)";
+      button.style.backgroundSize = "200% 100%";
+      button.style.backgroundPosition = "100% 0";
+    });
+    // Then set clicked button to new color
+    event.target.style.background =
+      "linear-gradient(to right, #1babff 0%, #41c4ce 50%, rgb(2, 168, 177) 50%, rgb(2, 168, 177) 100%)";
+    event.target.style.backgroundSize = "200% 100%";
+    event.target.style.backgroundPosition = "100% 0";
+    // Update state to render new project
+    setSelect(button - 1);
+  };
   return (
     <Fragment>
+      <div className="other-projects-title">
+        <p className="other-title">Other Projects</p>
+      </div>
       <div className="mobile-projects">
         <div className="mobile-projects-info">
           <p className="mobile-projects-title">{data[select].title}</p>
@@ -34,6 +60,38 @@ const OtherProjectsM = () => {
             className="mobile-projects-img"
             src={data[select].image}
           ></img>
+        </div>
+        <div className="select-button-div">
+          <button
+            className="select-button select"
+            onClick={changeSelect}
+            onMouseEnter={enter}
+            onTouchStart={enter}
+            onTouchEnd={close}
+            onMouseLeave={close}
+          >
+            1
+          </button>
+          <button
+            className="select-button select"
+            onClick={changeSelect}
+            onMouseEnter={enter}
+            onTouchStart={enter}
+            onTouchEnd={close}
+            onMouseLeave={close}
+          >
+            2
+          </button>
+          <button
+            className="select-button select"
+            onClick={changeSelect}
+            onMouseEnter={enter}
+            onTouchStart={enter}
+            onTouchEnd={close}
+            onMouseLeave={close}
+          >
+            3
+          </button>
         </div>
       </div>
     </Fragment>

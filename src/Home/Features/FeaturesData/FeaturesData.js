946780abadd32435bf0943bd1390e27050695d15
Brandon <37314885+Sycendin@users.noreply.github.com>
2022-07-13 04:49:45 -0400

    style: update style

--- src/Home/Features/FeaturesData/FeaturesData.js
+++ src/Home/Features/FeaturesData/FeaturesData.js
@@ -1,19 +1,19 @@
 export const dataGame = [
   {
-    title: "Disqus",
-    text: "Uses disqus to comment",
+    title: "Disqus Coment Section",
+    text: "Webite uses react-disqus which embedds a disquis comment section the all cards in archetype pages. #break Using a your disqus account, you can comment under an archetype",
     image:
       "https://cdn.discordapp.com/attachments/747928182891610213/996327638744432671/disqus.webp",
   },
   {
     title: "YGOPRODECK API",
-    text: "Uses YGOPRODECKAPI",
+    text: "Uses YGOPRODECK API to get a random card, search and get card images.",
     image:
       "https://cdn.discordapp.com/attachments/747928182891610213/996327638526349342/ygoprodeck.webp",
   },
   {
     title: "Web routing",
-    text: "Uses YGOPRODECKAPI",
+    text: "Uses react-router-dom to route through each page in the website. #break",
     image:
       "https://cdn.discordapp.com/attachments/747928182891610213/996327638299840602/routing.webp",
   },

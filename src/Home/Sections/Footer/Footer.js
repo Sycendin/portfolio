3ae5e07e4690ef902043a23223044c512c14ed85
Brandon <37314885+Sycendin@users.noreply.github.com>
2022-08-05 00:12:29 -0400

    feat: add linkedin link

--- src/Home/Sections/Footer/Footer.js
+++ src/Home/Sections/Footer/Footer.js
@@ -40,6 +40,19 @@ const Footer = () => {
                   window.open("https://github.com/Sycendin", "_blank")
                 }
               ></img>
+              <img
+                alt="linkedin"
+                className="footer-img"
+                width={100}
+                height={100}
+                src="https://cdn.discordapp.com/attachments/788247984517283880/1004958551908827249/linkedin.webp"
+                onClick={() =>
+                  window.open(
+                    "https://www.linkedin.com/in/brandon-shewnarain-47167922a/",
+                    "_blank"
+                  )
+                }
+              ></img>
             </div>
           </div>
           <p className="footer-text">Website Developed By Brandon Shewnarain</p>

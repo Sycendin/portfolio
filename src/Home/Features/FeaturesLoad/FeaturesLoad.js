b71afeb9273d8f223ee3428bcec5812b2c2ac170
Brandon <37314885+Sycendin@users.noreply.github.com>
2022-09-16 00:15:45 -0400

    update: add intial implementation of featureload

--- src/Home/Features/FeaturesLoad/FeaturesLoad.js
+++ src/Home/Features/FeaturesLoad/FeaturesLoad.js
@@ -1,7 +1,16 @@
 import React, { Fragment } from "react";
 
 const FeaturesLoad = () => {
-  return <Fragment></Fragment>;
+  return (
+    <Fragment>
+      <div className="fwhole-div">
+        <div className="fload-p"></div>
+        <div className="fload-div"></div>
+        <div className="fload-div"></div>
+        <div className="fload-div"></div>
+      </div>
+    </Fragment>
+  );
 };
 
 export default FeaturesLoad;

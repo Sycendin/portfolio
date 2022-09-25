ba950d4342ebc950736ac93567570ab23ed02882
Brandon <37314885+Sycendin@users.noreply.github.com>
2022-09-25 00:09:02 -0400

    update: add initial files for OProjectload

---
+++ src/Home/Sections/OtherProjects/OProjectsLoad/OProjectsLoad.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Fragment } from "react";
+const OProjectsLoad = () => {
+  return (
+    <Fragment>
+      <div className="oproject-wholediv"></div>
+    </Fragment>
+  );
+};
+export default OProjectsLoad;

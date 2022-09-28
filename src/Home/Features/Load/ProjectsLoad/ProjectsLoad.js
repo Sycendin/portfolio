77bf3e9a4390b4d08523d4995d2602a8e12bdab2
Brandon <37314885+Sycendin@users.noreply.github.com>
2022-09-28 01:39:28 -0400

    style: improve style for loadscreen for projects

--- src/Home/Features/Load/ProjectsLoad/ProjectsLoad.js
+++ src/Home/Features/Load/ProjectsLoad/ProjectsLoad.js
@@ -1,5 +1,5 @@
 import React, { Fragment } from "react";
-
+import "./ProjectsLoad.css";
 const ProjectsLoad = () => {
   return (
     <Fragment>
@@ -10,10 +10,17 @@ const ProjectsLoad = () => {
         {[1, 2, 3].map((element) => {
           return (
             <div key={element} className="pload-div fmargin fbg">
-              <div className="pload-innerleft">
-                <div className="pload-button"></div>
+              <div className="pload-innerleft fbg">
+                <div className="left-content">
+                  <div className="pload-text fbgd"></div>
+                  <div className="pload-button-div">
+                    <div className="pload-button fbgd"></div>
+                    <div className="pload-button fbgd"></div>
+                    <div className="pload-button fbgd"></div>
+                  </div>
+                </div>
               </div>
-              <div className="pload-innerright"></div>
+              <div className="pload-innerright fbg"></div>
             </div>
           );
         })}

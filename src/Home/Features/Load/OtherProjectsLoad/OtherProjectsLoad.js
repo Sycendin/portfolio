f640e4ef00d42fc188eab7567f9c70265780ecc2
Brandon <37314885+Sycendin@users.noreply.github.com>
2022-09-30 00:05:32 -0400

    style: improve other projects loading screen

--- src/Home/Features/Load/OtherProjectsLoad/OtherProjectsLoad.js
+++ src/Home/Features/Load/OtherProjectsLoad/OtherProjectsLoad.js
@@ -6,13 +6,39 @@ const OtherProjectsLoad = () => {
       <div className="oproject-wholediv">
         <div className="oproject-divl ">
           <div className="oproject-div ">
+            <div className="oproject-both">
+              <div className="oproject-inner-left">
+                <div className="left-contents">
+                  <div className="oproject-text "></div>
+                  <div className="button-contents">
+                    <div className="oproject-button "></div>
+                    <div className="oproject-button "></div>
+                  </div>
+                </div>
+              </div>
+
+              <div className="oproject-inner-right">
+                <div className="oproject-img"></div>
+              </div>
+            </div>
+            {/* 
+            <div className="oproject-divinnerr "></div> */}
+          </div>
+
+          <div className="oproject-div ">
             {/* <div className="oproject-divinnerl ">
               <div className="oproject-text "></div>
             </div>
 
             <div className="oproject-divinnerr "></div> */}
           </div>
+          <div className="oproject-div ">
+            {/* <div className="oproject-divinnerl ">
+              <div className="oproject-text "></div>
+            </div>
 
+            <div className="oproject-divinnerr "></div> */}
+          </div>
           <div className="oproject-div ">
             {/* <div className="oproject-divinnerl ">
               <div className="oproject-text "></div>
